5f32231eb0aca607c0e9bb54ba22b81a7feda7e3
youseft42 <43215321+youseft42@users.noreply.github.com>
2021-04-11 21:31:32 +0300

    Add files via upload

--- implementations/models.py
+++ implementations/models.py
@@ -2,11 +2,60 @@ import re
 import torch
 import torch.nn as nn
 import torch.utils.data
+import math
 from torch import Tensor
 from typing import Iterator
 
+def create_emb_layer(weights_matrix):
+    num_embeddings, embedding_dim = weights_matrix.size()
+    weight = torch.FloatTensor(weights_matrix)
+    emb_layer = nn.Embedding.from_pretrained(weight)
+    # emb_layer.load_state_dict({'weight': weights_matrix})
+    return emb_layer, num_embeddings, embedding_dim
+
+
+class PositionalEncoder(nn.Module):
+    def __init__(self, d_model, max_seq_len=80):
+        super().__init__()
+        self.d_model = d_model
+
+        # create constant 'pe' matrix with values dependant on
+        # pos and i
+        pe = torch.zeros(max_seq_len, d_model)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1)) / d_model)))
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # make embeddings relatively larger
+        x = x * math.sqrt(self.d_model)
+        # add constant to embedding
+        x = x.transpose(0, 1)
+        seq_len = x.shape[1]
+        x = x + torch.Tensor(self.pe[:, :seq_len, :])
+        x = x.transpose(0, 1)
+        # print(x.shape)
+        return x
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff=2048, dropout=0.1):
+        super().__init__()
+        # We set d_ff as a default to 2048
+        self.dropout = nn.Dropout(dropout)
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+    def forward(self, x):
+        x = self.dropout(x)
+        x = torch.relu(self.linear_1(x))
+        x = torch.relu(self.linear_2(x))
+        return x
+
 class SubjectivityLSTM(nn.Module):
-    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0):
+    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0.1):
         """
         :param vocab_dim: Number of vocabulary dimensions.
         :param h_dim: Number of hidden state dimensions.
@@ -36,7 +85,7 @@ class SubjectivityLSTM(nn.Module):
 
 
 class StanceLSTM(nn.Module):
-    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0):
+    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0.1):
         """
         :param vocab_dim: Number of vocabulary dimensions.
         :param h_dim: Number of hidden state dimensions.
@@ -51,7 +100,7 @@ class StanceLSTM(nn.Module):
         # the second phase:
         self.embedding = nn.Embedding(vocab_dim, embedding_dim)
 
-        self.attn = torch.nn.MultiheadAttention(h_dim, h_dim)
+        self.attn = torch.nn.MultiheadAttention(h_dim, 1)
 
         self.lstm = nn.LSTM(input_size=embedding_dim + h_dim*n_layers, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
 
@@ -85,10 +134,10 @@ class TwoPhaseLSTM(nn.Module):
     Represents a two-phase LSTM model.
     """
 
-    def __init__(self, subLSTM: SubjectivityLSTM, stanceLSTM: StanceLSTM):
+    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0.1):
         super().__init__()
-        self.sub = subLSTM
-        self.stance = stanceLSTM
+        self.sub = SubjectivityLSTM(vocab_dim, embedding_dim, h_dim, n_layers, dropout)
+        self.stance = StanceLSTM(vocab_dim, embedding_dim, h_dim, n_layers, dropout)
         self.log_softmax = nn.LogSoftmax(dim=2)
 
     def forward(self, x_src, **kw):
@@ -101,9 +150,8 @@ class TwoPhaseLSTM(nn.Module):
         # Output shape: (S, B, 3)
         return y_stance, y_sub
 
-
-class SubjectivityGRU(nn.Module):
-    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0):
+class PeSubjectivityLSTM(nn.Module):
+    def __init__(self, weights_matrix, h_dim, n_layers=1, dropout=0.1):
         """
         :param vocab_dim: Number of vocabulary dimensions.
         :param h_dim: Number of hidden state dimensions.
@@ -113,27 +161,29 @@ class SubjectivityGRU(nn.Module):
         disables.
         """
         super().__init__()
-        assert vocab_dim > 0 and h_dim > 0 and n_layers > 0
+        assert h_dim > 0 and n_layers > 0
 
         # the first phase:
-        self.embedding = nn.Embedding(vocab_dim, embedding_dim)
+        self.embedding, num_embeddings, embedding_dim = create_emb_layer(weights_matrix)
 
-        self.lstm = nn.GRU(input_size=embedding_dim, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
+        # self.linear1 = nn.Linear(embedding_dim, h_dim)
 
-        self.linear = nn.Linear(h_dim, 3)
+        # self.pe = PositionalEncoder(embedding_dim)
+
+        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
 
     def forward(self, x, **kw):
 
         embedded = self.embedding(x)
-
+        # embedded = self.linear1(embedded)
+        # embedded = self.pe(embedded)
         h, ht = self.lstm(embedded)
-        out = self.linear(h)
 
-        return out, h, ht
+        return h, ht
 
 
-class StanceGRU(nn.Module):
-    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0):
+class PeStanceLSTM(nn.Module):
+    def __init__(self, weights_matrix, h_dim, n_layers=1, dropout=0.1):
         """
         :param vocab_dim: Number of vocabulary dimensions.
         :param h_dim: Number of hidden state dimensions.
@@ -143,120 +193,34 @@ class StanceGRU(nn.Module):
         disables.
         """
         super().__init__()
-        assert vocab_dim > 0 and h_dim > 0 and n_layers > 0
+        assert h_dim > 0 and n_layers > 0
 
         # the second phase:
-        self.embedding = nn.Embedding(vocab_dim, embedding_dim)
+        self.embedding, num_embeddings, embedding_dim = create_emb_layer(weights_matrix)
 
-        self.attn = torch.nn.MultiheadAttention(h_dim, h_dim)
+        # self.linear1 = nn.Linear(embedding_dim, h_dim)
 
-        self.gru = nn.GRU(input_size=embedding_dim + h_dim * n_layers, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
+        self.ff = FeedForward(h_dim, h_dim*2)
 
-        self.linear = nn.Linear(h_dim, 3)
+        self.attn = torch.nn.MultiheadAttention(h_dim, 1)
+
+        self.lstm = nn.LSTM(input_size=embedding_dim + h_dim*n_layers, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
+
+        # self.linear = nn.Linear(h_dim, 3)
 
     def forward(self, x, h_prev, sub_h, **kw):
         S, B = x.shape
         embedded = self.embedding(x)  # embedded shape: (S, B, E)
+        # print(embedded.shape)
+        # embedded = self.linear1(embedded)
 
-        q = h_prev# (L,B, H)
+        # embedded = self.ff(embedded)
+        q = h_prev[0]# (L,B, H)
+        # print(q.shape)
+        # q = self.pe(q)
         kv = sub_h# (S, B, H)
-        a, _ = self.attn(q, kv, kv)  # (L, B , H)
-
-        a = a.reshape((1, B, -1)).expand(S, -1, -1)
-        rnn_input = torch.cat((embedded, a), dim=2)  # (S, B, E + L*H)
-
-        # h:  (S, B, H)
-        # ht: (L, B, H)
-        h, ht = self.gru(rnn_input, h_prev)
-
-        # Project H back to the output size to get prediction for the stance
-        out = self.linear(h)
-
-        # Out shapes: (S, B, V) and (L, B, H)
-        return out, ht
-
-
-class TwoPhaseGRU(nn.Module):
-
-    """
-    Represents a two-phase LSTM model.
-    """
-
-    def __init__(self, subGRU: SubjectivityGRU, stanceGRU: StanceGRU):
-        super().__init__()
-        self.sub = subGRU
-        self.stance = stanceGRU
-        self.log_softmax = nn.LogSoftmax(dim=2)
-
-    def forward(self, x_src, **kw):
-
-        # context is (L, B, H)
-        sub_output, h, context = self.sub(x_src, **kw)
-        stance_output, context = self.stance(x_src, context, h, **kw)
-        y_stance = self.log_softmax(stance_output)
-        y_sub = self.log_softmax(sub_output)
-        # Output shape: (S, B, 3)
-        return y_stance, y_sub
-
-
-class SubjectivityRNN(nn.Module):
-    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0):
-        """
-        :param vocab_dim: Number of vocabulary dimensions.
-        :param h_dim: Number of hidden state dimensions.
-        :param embedding_dim: Number of embedding dimensions.
-        :param n_layers: Number of layer in the model.
-        :param dropout: Level of dropout to apply between layers. Zero
-        disables.
-        """
-        super().__init__()
-        assert vocab_dim > 0 and h_dim > 0 and n_layers > 0
-
-        # the first phase:
-        self.embedding = nn.Embedding(vocab_dim, embedding_dim)
-
-        self.lstm = nn.RNN(input_size=embedding_dim, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
-
-        self.linear = nn.Linear(h_dim, 3)
-
-    def forward(self, x, **kw):
-
-        embedded = self.embedding(x)
-
-        h, ht = self.lstm(embedded)
-        out = self.linear(h)
-
-        return out, h, ht
-
-
-class StanceRNN(nn.Module):
-    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0):
-        """
-        :param vocab_dim: Number of vocabulary dimensions.
-        :param h_dim: Number of hidden state dimensions.
-        :param embedding_dim: Number of embedding dimensions.
-        :param n_layers: Number of layer in the model.
-        :param dropout: Level of dropout to apply between layers. Zero
-        disables.
-        """
-        super().__init__()
-        assert vocab_dim > 0 and h_dim > 0 and n_layers > 0
-
-        # the second phase:
-        self.embedding = nn.Embedding(vocab_dim, embedding_dim)
-
-        self.attn = torch.nn.MultiheadAttention(h_dim, h_dim)
-
-        self.lstm = nn.RNN(input_size=embedding_dim + h_dim*n_layers, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
-
-        self.linear = nn.Linear(h_dim, 3)
-
-    def forward(self, x, h_prev, sub_h, **kw):
-        S, B = x.shape
-        embedded = self.embedding(x)  # embedded shape: (S, B, E)
-
-        q = h_prev# (L,B, H)
-        kv = sub_h# (S, B, H)
+        kv = self.ff(kv)
+        # print(kv.shape)
         a, _ = self.attn(q, kv, kv)  # (L, B , H)
 
         a = a.reshape((1, B, -1)).expand(S, -1, -1)
@@ -267,30 +231,121 @@ class StanceRNN(nn.Module):
         h, ht = self.lstm(rnn_input, h_prev)
 
         # Project H back to the output size to get prediction for the stance
-        out = self.linear(h)
+        # out = self.linear(h)
 
         # Out shapes: (S, B, V) and (L, B, H)
-        return out, ht
+        return h, ht
 
 
-class TwoPhaseRNN(nn.Module):
+class PeTwoPhaseLSTM(nn.Module):
 
     """
     Represents a two-phase LSTM model.
     """
 
-    def __init__(self, subRNN: SubjectivityRNN, stanceRNN: StanceRNN):
+    def __init__(self, weights_matrix, h_dim, n_layers=1, dropout=0.1):
         super().__init__()
-        self.sub = subRNN
-        self.stance = stanceRNN
-        self.log_softmax = nn.LogSoftmax(dim=2)
+        self.sub = PeSubjectivityLSTM(weights_matrix, h_dim, n_layers, dropout)
+        self.stance = PeStanceLSTM(weights_matrix, h_dim, n_layers, dropout)
+        self.pe = PositionalEncoder(h_dim)
+        self.linear1 = nn.Linear(h_dim, 3)
+        self.log_softmax = nn.LogSoftmax(dim=1)
+        self.linear2 = nn.Linear(h_dim, 3)
+
 
     def forward(self, x_src, **kw):
-
+        S, B = x_src.shape
         # context is (L, B, H)
-        sub_output, h, context = self.sub(x_src, **kw)
-        stance_output, context = self.stance(x_src, context, h, **kw)
-        y_stance = self.log_softmax(stance_output)
+        h, context = self.sub(x_src, **kw)
+        stance_outputs = []
+        stance_input = x_src[[0], :]
+        h = self.pe(h)
+        for t in range(1, S):
+            # Feed the stance sequences of length 1 & save new context
+            # stance_output is (1, B, V)
+            stance_output, context = self.stance(stance_input, context, h, **kw)
+            stance_outputs.append(stance_output)
+            stance_input = x_src[[t], :]
+
+        stance_out = torch.cat(stance_outputs, dim=0)
+        stance_out = torch.mean(stance_out, 0)
+        out = self.linear1(stance_out)
+        y_stance = self.log_softmax(out)
+        h = torch.mean(h, 0)
+        sub_output = self.linear2(h)
         y_sub = self.log_softmax(sub_output)
-        # Output shape: (S, B, 3)
+        # Output shape: (B, 3)
         return y_stance, y_sub
+
+class BaseLineLSTM(nn.Module):
+    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0):
+        """
+        :param vocab_dim: Number of vocabulary dimensions.
+        :param h_dim: Number of hidden state dimensions.
+        :param embedding_dim: Number of embedding dimensions.
+        :param n_layers: Number of layer in the model.
+        :param dropout: Level of dropout to apply between layers. Zero
+        disables.
+        """
+        super().__init__()
+        assert vocab_dim > 0 and h_dim > 0 and n_layers > 0
+
+        # the first phase:
+        self.embedding = nn.Embedding(vocab_dim, embedding_dim)
+
+        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
+
+        self.linear = nn.Linear(h_dim, 3)
+
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x, **kw):
+
+        embedded = self.embedding(x)
+
+        h, ht = self.lstm(embedded)
+
+        h = torch.mean(h, 0)
+
+        out = self.linear(h)
+
+        out = self.log_softmax(out)
+
+        return out
+
+class BaseLineRNN(nn.Module):
+    def __init__(self, vocab_dim, embedding_dim, h_dim, n_layers=1, dropout=0.1):
+        """
+        :param vocab_dim: Number of vocabulary dimensions.
+        :param h_dim: Number of hidden state dimensions.
+        :param embedding_dim: Number of embedding dimensions.
+        :param n_layers: Number of layer in the model.
+        :param dropout: Level of dropout to apply between layers. Zero
+        disables.
+        """
+        super().__init__()
+        assert vocab_dim > 0 and h_dim > 0 and n_layers > 0
+
+        # the first phase:
+        self.embedding = nn.Embedding(vocab_dim, embedding_dim)
+
+        self.rnn = nn.RNN(input_size=embedding_dim, hidden_size=h_dim, num_layers=n_layers, dropout=dropout)
+
+        self.linear = nn.Linear(h_dim, 3)
+
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
+
+    def forward(self, x, **kw):
+
+        embedded = self.embedding(x)
+
+        h, ht = self.rnn(embedded)
+
+        h = torch.mean(h, 0)
+
+        out = self.linear(h)
+
+        out = self.log_softmax(out)
+
+        return out
--- implementations/training.py
+++ implementations/training.py
@@ -39,6 +39,62 @@ def train_two_phase_rnn(model, dl_train, optimizer, sub_loss_fn, stance_loss_fn,
                 break
     return losses
 
+def train_pe_two_phase_rnn(model, dl_train, optimizer, sub_loss_fn, stance_loss_fn, max_batches=None):
+    losses = []
+
+    with tqdm.tqdm(total=(max_batches if max_batches else len(dl_train)), file=sys.stdout) as pbar:
+        for idx_batch, batch in enumerate(dl_train, start=1):
+            x, y_stance, y_sub = batch.TWEET, batch.STANCE, batch.SENTIMENT
+
+            # Forward pass
+            # Output
+            y_stance_hat, y_sub_hat = model(x)
+
+            # Calculate loss
+            loss_stance = stance_loss_fn(y_stance_hat, y_stance)
+            loss_sub = sub_loss_fn(y_sub_hat, y_sub)
+            loss = loss_stance + loss_sub
+
+            # Update parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            losses.append(loss_stance.item())
+
+            pbar.update()
+            pbar.set_description(f'train loss={losses[-1]},')
+            if max_batches and idx_batch >= max_batches:
+                break
+    return losses
+
+def train_baseline_rnn(model, dl_train, optimizer, stance_loss_fn, max_batches=None):
+    losses = []
+
+    with tqdm.tqdm(total=(max_batches if max_batches else len(dl_train)), file=sys.stdout) as pbar:
+        for idx_batch, batch in enumerate(dl_train, start=1):
+            x, y_stance = batch.TWEET, batch.STANCE
+
+            # Forward pass
+            # Output
+            y_stance_hat = model(x)
+
+            # Calculate loss
+            loss_stance = stance_loss_fn(y_stance_hat, y_stance)
+            loss = loss_stance
+
+            # Update parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            losses.append(loss_stance.item())
+
+            pbar.update()
+            pbar.set_description(f'train loss={losses[-1]},')
+            if max_batches and idx_batch >= max_batches:
+                break
+    return losses
 
 def eval_two_phase_rnn(model, dl_test):
     sub_accuracies = []
@@ -70,4 +126,52 @@ def eval_two_phase_rnn(model, dl_test):
             stance_to = torch.FloatTensor(stance_accuracies)
             pbar.set_description(f' sentiment accuracy={torch.mean(sub_to)}, stance accuracy={torch.mean(stance_to)} ')
 
-    return sub_accuracies, stance_accuracies
+    return torch.Tensor(sub_accuracies).mean(), torch.Tensor(stance_accuracies).mean()
+
+def eval_pe_two_phase_rnn(model, dl_test):
+    sub_accuracies = []
+    stance_accuracies = []
+
+    with tqdm.tqdm(total=(len(dl_test)), file=sys.stdout) as pbar:
+        for idx_batch, batch in enumerate(dl_test):
+            x, y_stance, y_sub = batch.TWEET, batch.STANCE, batch.SENTIMENT
+
+            # Forward pass
+            with torch.no_grad():
+                y_stance_hat, y_sub_hat = model(x)
+
+            B, V = y_stance_hat.shape
+
+            y_sub_pred = torch.argmax(y_sub_hat, dim=1)
+            y_stance_pred = torch.argmax(y_stance_hat, dim=1)
+
+            sub_accuracies.append(torch.sum(y_sub_pred == y_sub) / float(B))
+            stance_accuracies.append(torch.sum(y_stance_pred == y_stance) / float(B))
+            pbar.update()
+            sub_to = torch.FloatTensor(sub_accuracies)
+            stance_to = torch.FloatTensor(stance_accuracies)
+            pbar.set_description(f' sentiment accuracy={torch.mean(sub_to)}, stance accuracy={torch.mean(stance_to)} ')
+
+    return torch.Tensor(sub_accuracies).mean(), torch.Tensor(stance_accuracies).mean()
+
+def eval_baseline_rnn(model, dl_test):
+    stance_accuracies = []
+
+    with tqdm.tqdm(total=(len(dl_test)), file=sys.stdout) as pbar:
+        for idx_batch, batch in enumerate(dl_test):
+            x, y_stance = batch.TWEET, batch.STANCE
+
+            # Forward pass
+            with torch.no_grad():
+                y_stance_hat = model(x)
+
+            B, V = y_stance_hat.shape
+
+            y_stance_pred = torch.argmax(y_stance_hat, dim=1)
+
+            stance_accuracies.append(torch.sum(y_stance_pred == y_stance) / float(B))
+            pbar.update()
+            stance_to = torch.FloatTensor(stance_accuracies)
+            pbar.set_description(f' stance accuracy={torch.mean(stance_to)} ')
+
+    return torch.Tensor(stance_accuracies).mean()
